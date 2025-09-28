0557f41e943015e9a92609d36876078c70729646
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-09-28 20:15:56 +0500

    Task3

---
+++ Tyuiu.PozdeevaEA.Sprint1.Task3.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PozdeevaEA.Sprint1.Task3.V18.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint1.Task3.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double a = 8;
+            double b = 10;
+            double c = 2;
+            double wait = 20;
+            var res = ds.HowManySquares(a, b, c);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PozdeevaEA.Sprint1.Task3.V18/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.PozdeevaEA.Sprint1.Task3.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Поздеева Е. А. | ПКТб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Поздеева Елизавета Александровна | ПКТб-25-1                  *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет, сколько квадратов со стороной C  *");
+        Console.WriteLine("* можно разместить внутри прямоугольника с размерами A x B без наложений. *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double a, b, c;
+
+        Console.WriteLine("Введите значение стороны a прямоугольника");
+        a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение стороны b прямоугольника");
+        b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение стороны c прямоугольника");
+        c = Convert.ToDouble(Console.ReadLine());
+     
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(Math.Round(ds.HowManySquares(a, b, c), 3));
+
+        Console.ReadLine();
+    }
+}
