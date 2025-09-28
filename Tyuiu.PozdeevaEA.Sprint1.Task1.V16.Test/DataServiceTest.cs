37e75ea9a1f564ebd486c9a5d451b0267662b641
PozdeevaElizaveta <Tyuiu.PozdeevaEA@outlook.com>
2025-09-28 17:09:02 +0500

    Task1

---
+++ Tyuiu.PozdeevaEA.Sprint1.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PozdeevaEA.Sprint1.Task1.V16.Lib;
+namespace Tyuiu.PozdeevaEA.Sprint1.Task1.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double x = 2.0;
+            double a = 1.0;
+            double y = 3.0;
+            
+            var res = ds.Calculate(x, a, y);
+            Assert.AreEqual(16, res);
+        }
+    }
+}
